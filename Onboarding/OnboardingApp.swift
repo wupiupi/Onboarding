d003217ea29e22d60b25a87e160756648151cf6f
Paul <pasha.makey@gmail.com>
2024-06-18 09:49:48 +0300

    Initial Commit

---
+++ Onboarding/OnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingApp.swift
+//  Onboarding
+//
+//  Created by Paul Makey on 18.06.24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
