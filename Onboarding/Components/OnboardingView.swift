b7885165b7d3fda86d8bba37206ca638a5474070
Paul <pasha.makey@gmail.com>
2024-06-19 20:55:08 +0300

    Added New Files

---
+++ Onboarding/Components/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  Onboarding
+//
+//  Created by Paul Makey on 19.06.24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
---
+++ Onboarding/Extensions/Extension + Color.swift
@@ -0,0 +1,8 @@
+//
+//  Extension + Color.swift
+//  Onboarding
+//
+//  Created by Paul Makey on 19.06.24.
+//
+
+import Foundation
