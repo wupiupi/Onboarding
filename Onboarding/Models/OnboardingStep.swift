848c95a551e99ce531a9d08e871c913921f8a45b
Paul <pasha.makey@gmail.com>
2024-06-19 19:10:49 +0300

    OnboardingStep Model Created

---
+++ Onboarding/Models/OnboardingStep.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingStep.swift
+//  Onboarding
+//
+//  Created by Paul Makey on 19.06.24.
+//
+
+struct OnboardingStep {
+    let image: String
+    let title: String
+    let description: String
+}
