aa8eda153d3e60b01b19652ec6f6f072539655af
Paul <pasha.makey@gmail.com>
2024-06-20 00:40:04 +0300

    Added HomeView

---
+++ Onboarding/Views/HomeView/HomeView.swift
@@ -0,0 +1,25 @@
+//
+//  HomeView.swift
+//  Onboarding
+//
+//  Created by Paul Makey on 20.06.24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        ZStack {
+            Color(hex: 0x95B6FF)
+            
+            Text("You are a clever person!")
+                .foregroundStyle(.white)
+                .font(.title2)
+                .fontWeight(.black)
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
